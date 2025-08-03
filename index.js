2186651af2052c6e0d1d06b6eaa286fb7ca9b8da
Johnson <sakajohnson1@gmail.com>
2025-08-03 11:11:38 +0000

    Organised code into functions that handle different tasks

--- index.js
+++ index.js
@@ -1,5 +1,116 @@
-//alert("Hello walker")
+
+
 let extensionsData = [];
+
+function loadExtensionsData(){
+  const savedData = localStorage.getItem("extensionsData");
+
+  if (savedData){
+    return  Promise .resolve(JSON.parse(savedData));
+  } else {
+    fetch("./data.json")
+      .then(response => response.json)
+      .then(data => extensionsData = data)
+      .catch(error => console.error("Error fetching data" , error))
+      return [];
+  }
+}
+
+
+function renderExtensions(list){
+  const container = document.getElementById("extension-list")
+
+  container.innerHTML = '';
+  list.forEach(extension => {
+    const item = createExtensionItem(extension);
+    container.appendChild(item);
+  });
+}
+
+
+function createExtensionItem(extension){
+  const item = document.createElement("div");
+  item.classList.add("grid-item");
+
+  item.innerHTML = `
+    <div class="content">
+      <div class="content-image">
+        <img src="${extension.logo}" alt="${extension.name}">
+      </div>
+      <div class="content-description">
+        <h3>${extension.name}</h3>
+        <p>${extension.description}</p>
+      </div>
+    </div>
+    <div class="content_footer">
+      <button class="remove_btn" type="button">Remove</button>
+      <div class="custom-switch ${extension.active ? 'active' : ''}">
+        <div class="slider"></div>
+      </div>
+    </div>
+  `;
+
+  addToggleEvent(item,extension);
+  addRemoveEvent(item, extension);
+
+  return item ;
+}
+
+function addToggleEvent(item, extension){
+  const toggle = item.querySelector(".custom-switch");
+
+  toggle.addEventListener("click" , () => {
+    extension.active = !extension.active;
+
+    saveExtensionsData();
+  })
+}
+
+
+function addRemoveEvent(item,extension){
+  const RemoveBtn = item.querySelector(".remove_btn");
+  RemoveBtn.addEventListener("click" , () => {
+    extensionsData = extensionsData.filter(e => e!== extension);
+
+    saveExtensionsData();
+    renderExtensions(extensionsData);
+  })
+}
+
+function saveExtensionsData(){
+  localStorage.setItem("extensions" , JSON.stringify(extensionsData));
+}
+
+function setupFilters(){
+  document.querySelectorAll(".filter_btn").forEach(button => {
+    button.addEventListener("click" , () => {
+      document.querySelectorAll(".filter_btn").forEach(btn => btn.classList.remove('active'));
+      button.classList.add('active');
+
+
+      const filter = button.dataset.filter;
+      if(filter === "all"){
+        renderExtensions(extensionsData);
+      } else if (filter === "active"){
+        renderExtensions(extensionsData.filter(item => item.active));
+      }else if (filter === "inactive"){
+        renderExtensions(extensionsData.filter(item => !item.active));
+      }
+    });
+  });
+}
+
+
+function initializeApp(){
+  loadExtensionsData().then(data => {
+    extensionsData = data;
+    renderExtensions(extensionsData);
+    setupFilters();
+  })
+}
+
+initializeApp();
+/*let extensionsData = [];
 
 
 // load JSON data
@@ -43,7 +154,7 @@ function renderExtensions(list){
           <button class="remove_btn" type="button">Remove</button>
           <div class="custom-switch ${extension.active ? 'active' : ''}">
             <div class="slider"></div>
-          </div>
+        </div>
     `;
     const toggle = item.querySelector(".custom-switch");
 
@@ -95,3 +206,4 @@ document.querySelectorAll('.filter-btn').forEach(button => {
 
 
 
+*/
